99a1540d98a765ae3378881a8cebe97fbe42ab25
damiantirapegui <damiantirapegui@gmail.com>
2024-08-08 19:45:31 +0200

    callback que

--- script.js
+++ script.js
@@ -243,8 +243,21 @@ const renderCountry = function (data, className = '') {
 // https://geocode.xyz/${lat},${lng}?geoit=json
 // https://restcountries.com/v2/name/${data.country}
 
-const whereAmI = function (lat, lng) {
-  fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`).then(response => {
-    return response.json();
-  });
-};
+// const whereAmI = function (lat, lng) {
+//   fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`).then(response => {
+//     return response.json();
+//   });
+// };
+
+console.log('start test');
+setTimeout(() => console.log('0 sec timer'), 0);
+Promise.resolve('Resolved promise 1').then(res => {
+  console.log(res);
+});
+
+Promise.resolve('Resolved promise 2').then(res => {
+  for (let i = 0; i < 100000000; i++) {}
+  console.log(res);
+});
+
+console.log('Test end');
